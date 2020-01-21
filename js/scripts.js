1eb62aa521ff9bb2062d2a69bc8dcfb8b359535e
Student <example@example.com>
2020-01-21 11:27:07 -0800

    refactor jquery to include loops

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,37 @@
 $(document).ready(function()	{
-	$("#formOne").submit(function()	 {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput = $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val(); 
+	$("#formOne").submit(function(event)	 {
+		
+		var userInputs = ["name", "otherName", "animal", "exclamation", "verb", "noun"];
+		
+		userInputs.forEach(function(userInput)	{
+			 var word = $("input#" + userInput).val(); 
+			 $("." + userInput).text(word);
+		});
+
+		$("#story").show();
+
+
+
+
+
+
+        
+    // var person1Input = $("input#person1").val();
+    // var person2Input = $("input#person2").val();
+    // var animalInput = $("input#animal").val();
+    // var exclamationInput = $("input#exclamation").val();
+    // var verbInput = $("input#verb").val();
+    // var nounInput = $("input#noun").val(); 
    
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    // $(".person1").text(person1Input);
+    // $(".person2").text(person2Input);
+    // $(".animal").text(animalInput);
+    // $(".exclamation").text(exclamationInput);
+    // $(".verb").text(verbInput);
+    // $(".noun").text(nounInput);
 
-    $("#story").show();
-
+    // $("#story").show();
+    
     event.preventDefault();
 	});
 });
